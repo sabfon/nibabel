ec792380add0193ced611dee572c38ffeaae7055
Thomas Emmerling <git@thomasemmerling.de>
2013-12-12 13:03:47 +0100

    RF: BvFileHeader inherits from LabeledWrapStruct

--- nibabel/bv.py
+++ nibabel/bv.py
@@ -38,7 +38,7 @@ class BvError(Exception):
     """
     pass
 
-class BvFileHeader(object):
+class BvFileHeader(LabeledWrapStruct):
     """Class to hold information from a BV file header.
     """
 
@@ -49,6 +49,8 @@ class BvFileHeader(object):
     # data scaling capabilities
     has_data_slope = False
     has_data_intercept = False
+
+    _field_recoders = {'datatype': data_type_codes}
 
     def __init__(self,
                  binaryblock=None,
@@ -100,144 +102,20 @@ class BvFileHeader(object):
 
         Returns
         -------
-        wstr : WrapStruct object
-           WrapStruct object initialized from data in fileobj
+        header : BvFileHeader object
+           BvFileHeader object initialized from data in fileobj
         '''
         raw_str = fileobj.read(10000)
         return klass(raw_str, endianness, check)
-
-    @property
-    def binaryblock(self):
-        ''' binary block of data as string
-
-        Returns
-        -------
-        binaryblock : string
-            string giving binary data block
-
-        Examples
-        --------
-        >>> # Make default empty structure
-        >>> wstr = WrapStruct()
-        >>> len(wstr.binaryblock)
-        2
-        '''
-        structarr = self._structarr
-
-        return self._structarr.tostring()
-
-    def write_to(self, fileobj):
-        ''' Write structure to fileobj
-
-        Write starts at fileobj current file position.
-
-        Parameters
-        ----------
-        fileobj : file-like object
-           Should implement ``write`` method
-
-        Returns
-        -------
-        None
-
-        Examples
-        --------
-        >>> wstr = WrapStruct()
-        >>> from io import BytesIO
-        >>> str_io = BytesIO()
-        >>> wstr.write_to(str_io)
-        >>> wstr.binaryblock == str_io.getvalue()
-        True
-        '''
-        fileobj.write(self.binaryblock)
-
-    @property
-    def endianness(self):
-        ''' endian code of binary data
-
-        The endianness code gives the current byte order
-        interpretation of the binary data.
-
-        Examples
-        --------
-        >>> wstr = WrapStruct()
-        >>> code = wstr.endianness
-        >>> code == native_code
-        True
-
-        Notes
-        -----
-        Endianness gives endian interpretation of binary data. It is
-        read only because the only common use case is to set the
-        endianness on initialization, or occasionally byteswapping the
-        data - but this is done via the as_byteswapped method
-        '''
-        if self._structarr.dtype.isnative:
-            return native_code
-        return swapped_code
-
-    def copy(self):
-        ''' Return copy of structure
-
-        >>> wstr = WrapStruct()
-        >>> wstr['integer'] = 3
-        >>> wstr2 = wstr.copy()
-        >>> wstr2 is wstr
-        False
-        >>> wstr2['integer']
-        array(3, dtype=int16)
-        '''
-        return self.__class__(
-                self.binaryblock,
-                self.endianness, check=False)
-
-    def __eq__(self, other):
-        ''' equality between two structures defined by binaryblock
-
-        Examples
-        --------
-        >>> wstr = WrapStruct()
-        >>> wstr2 = WrapStruct()
-        >>> wstr == wstr2
-        True
-        >>> wstr3 = WrapStruct(endianness=swapped_code)
-        >>> wstr == wstr3
-        True
-        '''
-        this_end = self.endianness
-        this_bb = self.binaryblock
-        try:
-            other_end = other.endianness
-            other_bb = other.binaryblock
-        except AttributeError:
-            return False
-        if this_end == other_end:
-            return this_bb == other_bb
-        other_bb = other._structarr.byteswap().tostring()
-        return this_bb == other_bb
-
-    def __ne__(self, other):
-        return not self == other
-
-    def __getitem__(self, item):
-        ''' Return values from structure data
-
-        Examples
-        --------
-        >>> wstr = WrapStruct()
-        >>> wstr['integer'] == 0
-        True
-        '''
-        return self._structarr[item]
 
     def __setitem__(self, item, value):
         ''' Set values in structured data
 
         Examples
         --------
-        >>> wstr = WrapStruct()
-        >>> wstr['integer'] = 3
-        >>> wstr['integer']
+        >>> hdr = BvFileHeader()
+        >>> hdr['integer'] = 3
+        >>> hdr['integer']
         array(3, dtype=int16)
         '''
         if self.template_dtype[item].type == np.string_:
@@ -249,197 +127,6 @@ class BvFileHeader(object):
                 wstr[key] = self._structarr[key]
             self._structarr = wstr.copy()
         self._structarr[item] = value
-
-    def __iter__(self):
-        return iter(self.keys())
-
-    def keys(self):
-        ''' Return keys from structured data'''
-        return list(self.template_dtype.names)
-
-    def values(self):
-        ''' Return values from structured data'''
-        data = self._structarr
-        return [data[key] for key in self.template_dtype.names]
-
-    def items(self):
-        ''' Return items from structured data'''
-        return zip(self.keys(), self.values())
-
-    def get(self, k, d=None):
-        ''' Return value for the key k if present or d otherwise'''
-        return (k in self.keys()) and self._structarr[k] or d
-
-    def check_fix(self, logger=None, error_level=None):
-        ''' Check structured data with checks '''
-        if logger is None:
-            logger = imageglobals.logger
-        if error_level is None:
-            error_level = imageglobals.error_level
-        battrun = BatteryRunner(self.__class__._get_checks())
-        self, reports = battrun.check_fix(self)
-        for report in reports:
-            report.log_raise(logger, error_level)
-
-    @classmethod
-    def diagnose_binaryblock(klass, binaryblock, endianness=None):
-        ''' Run checks over binary data, return string '''
-        wstr = klass(binaryblock, endianness=endianness, check=False)
-        battrun = BatteryRunner(klass._get_checks())
-        reports = battrun.check_only(wstr)
-        return '\n'.join([report.message
-                          for report in reports if report.message])
-
-    @classmethod
-    def guessed_endian(self, mapping):
-        ''' Guess intended endianness from mapping-like ``mapping``
-
-        Parameters
-        ----------
-        wstr : mapping-like
-            Something implementing a mapping.  We will guess the endianness from
-            looking at the field values
-
-        Returns
-        -------
-        endianness : {'<', '>'}
-           Guessed endianness of binary data in ``wstr``
-        '''
-        raise NotImplementedError
-
-    @property
-    def structarr(self):
-        ''' Structured data, with data fields
-
-        Examples
-        --------
-        >>> wstr1 = WrapStruct() # with default data
-        >>> an_int = wstr1.structarr['integer']
-        >>> wstr1.structarr = None
-        Traceback (most recent call last):
-           ...
-        AttributeError: can't set attribute
-        '''
-        return self._structarr
-
-    def as_byteswapped(self, endianness=None):
-        ''' return new byteswapped object with given ``endianness``
-
-        Guaranteed to make a copy even if endianness is the same as
-        the current endianness.
-
-        Parameters
-        ----------
-        endianness : None or string, optional
-           endian code to which to swap.  None means swap from current
-           endianness, and is the default
-
-        Returns
-        -------
-        wstr : ``WrapStruct``
-           ``WrapStruct`` object with given endianness
-
-        Examples
-        --------
-        >>> wstr = WrapStruct()
-        >>> wstr.endianness == native_code
-        True
-        >>> bs_wstr = wstr.as_byteswapped()
-        >>> bs_wstr.endianness == swapped_code
-        True
-        >>> bs_wstr = wstr.as_byteswapped(swapped_code)
-        >>> bs_wstr.endianness == swapped_code
-        True
-        >>> bs_wstr is wstr
-        False
-        >>> bs_wstr == wstr
-        True
-
-        If you write to the resulting byteswapped data, it does not
-        change the original.
-
-        >>> bs_wstr['integer'] = 3
-        >>> bs_wstr == wstr
-        False
-
-        If you swap to the same endianness, it returns a copy
-
-        >>> nbs_wstr = wstr.as_byteswapped(native_code)
-        >>> nbs_wstr.endianness == native_code
-        True
-        >>> nbs_wstr is wstr
-        False
-        '''
-        current = self.endianness
-        if endianness is None:
-            if current == native_code:
-                endianness = swapped_code
-            else:
-                endianness = native_code
-        else:
-            endianness = endian_codes[endianness]
-        if endianness == current:
-            return self.copy()
-        wstr_data = self._structarr.byteswap()
-        return self.__class__(wstr_data.tostring(),
-                              endianness,
-                              check=False)
-
-    def get_value_label(self, fieldname): #TODO
-        ''' Returns label for coded field
-
-        A coded field is an int field containing codes that stand for
-        discrete values that also have string labels.
-
-        Parameters
-        ----------
-        fieldname : str
-           name of header field to get label for
-
-        Returns
-        -------
-        label : str
-           label for code value in header field `fieldname`
-
-        Raises
-        ------
-        ValueError : if field is not coded
-
-        Examples
-        --------
-        >>> from nibabel.volumeutils import Recoder
-        >>> recoder = Recoder(((1, 'one'), (2, 'two')), ('code', 'label'))
-        >>> class C(LabeledWrapStruct):
-        ...     template_dtype = np.dtype([('datatype', 'i2')])
-        ...     _field_recoders = dict(datatype = recoder)
-        >>> hdr  = C()
-        >>> hdr.get_value_label('datatype')
-        '<unknown code 0>'
-        >>> hdr['datatype'] = 2
-        >>> hdr.get_value_label('datatype')
-        'two'
-        '''
-        if not fieldname in self._field_recoders:
-            raise ValueError('%s not a coded field' % fieldname)
-        code = int(self._structarr[fieldname])
-        try:
-            return self._field_recoders[fieldname].label[code]
-        except KeyError:
-            return '<unknown code {0}>'.format(code)
-
-    def __str__(self):
-        ''' Return string representation for printing '''
-        summary = "%s object, endian='%s'" % (self.__class__,
-                                              self.endianness)
-        def _getter(obj, key):
-            try:
-                return obj.get_value_label(key)
-            except ValueError:
-                return obj[key]
-
-        return '\n'.join(
-            [summary,
-             pretty_mapping(self, _getter)])
 
     @classmethod
     def default_structarr(klass):
@@ -630,7 +317,6 @@ class BvFileHeader(object):
             raise HeaderDataError(
                 'data dtype "%s" known but not supported' % datatype)
         self._structarr['datatype'] = code
-        self._structarr['bitpix'] = dtype.itemsize * 8
 
     def get_xflip(self):
         ''' Get xflip for data
