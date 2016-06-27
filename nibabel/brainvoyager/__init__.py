2953e98d29c9aa4513a9d715c00adb42989676b4
Thomas Emmerling <git@thomasemmerling.de>
2016-06-27 12:48:10 +0200

    BF: BvVmr __init__ imports correct.

--- nibabel/brainvoyager/__init__.py
+++ nibabel/brainvoyager/__init__.py
@@ -1,7 +1,9 @@
 from .bv_msk import BvMskHeader, BvMskImage
 from .bv_vmp import BvVmpHeader, BvVmpImage
 from .bv_vtc import BvVtcHeader, BvVtcImage
+from .bv_vmr import BvVmrHeader, BvVmrImage
 
 __all__ = ('BvMskHeader', 'BvMskImage',
            'BvVmpHeader', 'BvVmpImage',
-           'BvVtcHeader', 'BvVtcImage')
+           'BvVtcHeader', 'BvVtcImage',
+           'BvVmrHeader', 'BvVmrImage')
